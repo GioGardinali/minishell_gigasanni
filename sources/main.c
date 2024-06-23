8d5a481c6dc796ab5ded82ffb7545b5048a377a1
Adedayo-Sanni <adedayo.dc.sanni@gmail.com>
2024-06-23 16:42:18 -0300

    implementando a main parcial juntamente com a função start_minishell parcial rodando somente a readline

--- sources/main.c
+++ sources/main.c
@@ -6,8 +6,25 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 18:52:55 by asanni            #+#    #+#             */
-/*   Updated: 2024/06/23 16:05:54 by asanni           ###   ########.fr       */
+/*   Updated: 2024/06/23 16:41:22 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+void start_minishell(t_mini *minishell)
+{
+	minishell->input = readline("Minishelby> ");
+}
+
+int main (void)
+{
+	t_mini minishell;
+	minishell = (t_mini){0};
+	//minishell.env_args = getenv(__environ);
+	while (1)
+		start_minishell(&minishell);
+	//free_envs(minishell.env_args);
+	return (0);
+
+}
