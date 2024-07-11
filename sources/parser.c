01e11b2f88ff08dbe83db8499e93ebdfbeb61084
Adedayo-Sanni <adedayo.dc.sanni@gmail.com>
2024-07-11 20:27:11 -0300

    add descrição das built ins

--- sources/parser.c
+++ sources/parser.c
@@ -6,7 +6,7 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 20:21:55 by asanni            #+#    #+#             */
-/*   Updated: 2024/07/11 20:24:41 by asanni           ###   ########.fr       */
+/*   Updated: 2024/07/11 20:26:25 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,13 @@
 void	check_built_ins(t_mini *minishell, t_token *token)
 {
 }
+
+/* BUILT INS
+◦ echo with option -n
+◦ cd with only a relative or absolute path
+◦ pwd with no options
+◦ export with no options
+◦ unset with no options
+◦ env with no options or arguments
+◦ exit with no options
+*/
