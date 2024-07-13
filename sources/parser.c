a7111e4f27023e5d947ae532a68f81591579bdd0
Adedayo-Sanni <adedayo.dc.sanni@gmail.com>
2024-07-13 15:19:07 -0300

    ajustando argumentos da função check built ins

--- sources/parser.c
+++ sources/parser.c
@@ -6,13 +6,13 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 20:21:55 by asanni            #+#    #+#             */
-/*   Updated: 2024/07/13 15:16:56 by asanni           ###   ########.fr       */
+/*   Updated: 2024/07/13 15:18:30 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	check_built_ins(t_mini *minishell, t_token *token)
+void	check_built_ins(t_token *token)
 {
 	if (ft_strcmp(token->str, "echo"))
 		return ();
