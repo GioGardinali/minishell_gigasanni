b6779002edcc0559cd1a08cb944330bb77faa812
Adedayo <adedayo.dc.sanni@gmail.com>
2024-08-08 23:39:59 -0300

    add teste para multiplos cmds

--- sources/pipes.c
+++ sources/pipes.c
@@ -61,3 +61,81 @@ void	process_two_cmds(t_mini minishell)
 // 	execve(path, options, env);
 // 	exit(EXIT_FAILURE);
 // }
+
+/*
+teste
+
+
+void execute_command(t_cmd *cmd, int input_fd, int output_fd)
+{
+	if (input_fd != -1)
+	{
+		dup2(input_fd, STDIN_FILENO);
+		close(input_fd);
+	}
+	if (output_fd != -1)
+	{
+		dup2(output_fd, STDOUT_FILENO);
+		close(output_fd);
+	}
+	execve(cmd->path, cmd->options, __environ);
+	exit(EXIT_FAILURE);
+}
+
+pid_t fork_and_execute(t_cmd *cmd, int input_fd, int output_fd)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1)
+		exit(EXIT_FAILURE);
+	if (pid == 0)
+		execute_command(cmd, input_fd, output_fd);
+	return pid;
+}
+
+void create_pipe(int *fd)
+{
+	if (pipe(fd) == -1)
+		exit(EXIT_FAILURE);
+}
+
+void close_unused_fds(int input_fd, int *fd)
+{
+	if (input_fd != -1)
+		close(input_fd);
+	if (fd != NULL)
+	{
+		close(fd[1]);
+		input_fd = fd[0];
+	}
+}
+
+void process_multiple_cmds(t_mini minishell)
+{
+	int fd[2];
+	int prev_fd;
+	pid_t pid;
+	t_cmd *current_cmd;
+
+	prev_fd = -1;
+	current_cmd = minishell.cmd;
+	while (current_cmd != NULL)
+	{
+		if (current_cmd->next != NULL)
+			create_pipe(fd);
+		if (current_cmd->next == NULL)
+			pid = fork_and_execute(current_cmd, prev_fd, -1);
+		else
+			pid = fork_and_execute(current_cmd, prev_fd, fd[1]);
+		if (current_cmd->next != NULL)
+			close_unused_fds(prev_fd, fd);
+		else
+			close_unused_fds(prev_fd, NULL);
+		prev_fd = current_cmd->next != NULL ? fd[0] : -1;
+		current_cmd = current_cmd->next;
+	}
+	while (wait(NULL) > 0);
+}
+
+*/
