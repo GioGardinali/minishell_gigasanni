928d8497851c137165c537161d5328ddaa4121bc
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2024-08-13 19:48:26 -0300

    subindo função para remover aspas

--- sources/heredoc_utils.c
+++ sources/heredoc_utils.c
@@ -6,7 +6,7 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 18:30:41 by gigardin          #+#    #+#             */
-/*   Updated: 2024/07/31 19:57:03 by gigardin         ###   ########.fr       */
+/*   Updated: 2024/08/13 19:47:06 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,29 @@ int	check_quotes_in_token(char *str)
 		i++;
 	}
 	return (validate);
+}
+char	*remove_quotes(char *str_token)
+{
+	int		len;
+	char	*new;
+	char	*temp;
+	int		i;
+
+	if (!check_quotes_in_token(str_token))
+		return (ft_strdup(str_token));
+	len = ft_strlen(str_token);
+	new = ft_calloc(sizeof(char *), len + 1);
+	temp = new;
+	while (str_token[i])
+	{
+		if (str_token[i] != 34 && str_token[i] != 39)
+		{
+			*temp = str_token[i];
+			temp++;
+		}
+		i++;
+	}
+	return (new);
 }
 
 int unsigned count_cmd(t_token *temp_token)
