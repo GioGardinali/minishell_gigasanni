2c57b48ca3d57a547711f481a407a0ff97b22e07
Adedayo-Sanni <adedayo.dc.sanni@gmail.com>
2024-08-19 17:10:55 -0300

    add função split two

---
+++ libft/ft_split_two.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_two.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 20:40:08 by asanni            #+#    #+#             */
+/*   Updated: 2024/08/19 17:02:40 by asanni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	**print_wrds(char **str, const char *s, int c)
+{
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	len = 0;
+	while (s[len] != c && s[len])
+		len++;
+	str[i] = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str[i])
+		return (NULL);
+	ft_strlcpy(str[i], s, len + 1);
+	i++;
+	if (s[len] == c)
+	{
+		len++;
+		str[i] = (char *)malloc(sizeof(char) * (ft_strlen(s + len) + 1));
+		if (!str[i])
+			return (NULL);
+		ft_strlcpy(str[i], s + len, ft_strlen(s + len) + 1);
+		i++;
+	}
+	str[i] = NULL;
+	return (str);
+}
+
+char	**ft_split_two(char const *s, char c)
+{
+	char	**split;
+
+	if (!s)
+		return (NULL);
+	split = (char **)malloc(sizeof(char *) * 3);
+	if (!split)
+		return (NULL);
+	print_wrds(split, s, c);
+	return (split);
+}
