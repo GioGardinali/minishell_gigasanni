9031a2dbea98d19a4d87c3fced76a278225d0e4d
Adedayo-Sanni <adedayo.dc.sanni@gmail.com>
2024-09-01 17:21:18 -0300

    add pipes

--- sources/pipes.c
+++ sources/pipes.c
@@ -6,7 +6,7 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 15:48:38 by asanni            #+#    #+#             */
-/*   Updated: 2024/09/01 14:30:55 by asanni           ###   ########.fr       */
+/*   Updated: 2024/09/01 17:20:45 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ void	execute_command(t_mini minishell, int input_fd, int out_fd, t_cmd cmd)
 	}
 	free_token(&minishell.token);
 	free(minishell.input);
-//	free(minishell.env_content);
 	if (path != NULL)
 		execve(path, options, __environ);
 	exit(EXIT_FAILURE);
