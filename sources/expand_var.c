28bc6eae60ce0cd6cd0a8b77beceeb8a208fb17e
Adedayo-Sanni <adedayo.dc.sanni@gmail.com>
2024-09-12 20:22:57 -0300

    add expand_var funcionando

--- sources/expand_var.c
+++ sources/expand_var.c
@@ -6,7 +6,7 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 17:57:37 by asanni            #+#    #+#             */
-/*   Updated: 2024/09/11 20:15:18 by asanni           ###   ########.fr       */
+/*   Updated: 2024/09/12 20:19:27 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,106 +17,107 @@
 //ignore casos que não serão expandidos
 //cuidado com as aspas
 
-char	*str_rest(const char *str, const char *var_name)
+int	add_variable_size(t_mini *minishell, char *token, int *i)
 {
-	size_t	var_len;
-	size_t	rest_len;
-	char	*rest;
+	char	*var_key;
+	char	*var_value;
+	int		size;
 
-	var_len = ft_strlen(var_name);
-	rest_len = ft_strlen(str) - var_len;
-	if (ft_strncmp(str, var_name, var_len) == 0)
+	var_key = return_var(&token[*i + 1]);
+	size = 0;
+	if (var_key)
 	{
-		rest = ft_strdup(str + var_len);
-		return (rest);
+		var_value = return_key_content(minishell, var_key);
+		if (var_value)
+			size = ft_strlen(var_value);
+		*i += ft_strlen(var_key) + 1;
+		free(var_key);
 	}
-	return (NULL);
+	else
+	{
+		size = 1;
+		(*i)++;
+	}
+	return (size);
 }
 
-char	*test(t_mini *minishell, char *str)
+int	calculate_size(t_mini *minishell, char *token)
 {
-	char	*var;
-	char	*key_content;
-	char	*rest;
-	char	*exp;
+	int	total_size;
+	int	i;
 
-	var = return_var(str);
-	key_content = return_key_content(minishell, var);
-	rest = str_rest(str, var);
-	exp = ft_strjoin(key_content, rest);
-	return (exp);
+	total_size = 0;
+	i = 0;
+	while (token[i])
+	{
+		if (token[i] == '$' && is_valid(token[i + 1], 0))
+			total_size += add_variable_size(minishell, token, &i);
+		else
+		{
+			total_size++;
+			i++;
+		}
+	}
+	return (total_size);
 }
 
-// char	*expand_token(t_mini *minishell, char *token)
-// {
-// 	char	**temp;
-// 	char	*expanded;
-// 	char	*key_content;
-// 	int		i;
+void	append_to_result(char *result, char *str, int *j)
+{
+	int	len;
 
-// 	expanded = ft_strdup("");
-// 	i = 0;
-// 	if (ft_strchr(token, '$') == NULL)
-// 		return (token);
-// 	temp = ft_split(token, '$');
-// 	if (!temp)
-// 		return (NULL);
-// 	while (temp[i] != NULL)
-// 	{
-// 		key_content = test(minishell, temp[i]);
-// 		if (key_content != NULL)
-// 			expanded = ft_strjoin(expanded, key_content);
-// 		else
-// 			expanded = ft_strjoin(expanded, temp[i]);
-// 		i++;
-// 	}
-// 	free_matrix(temp);
-// 	return (expanded);
-// }
+	len = ft_strlen(str);
+	ft_strcpy(&result[*j], str);
+	*j += len;
+}
+
+int	expand_variable(t_mini *minishell, char *result, int *i, int *j)
+{
+	char	*var_key;
+	char	*var_value;
+
+	var_key = return_var(&minishell->token->str[*i + 1]);
+	if (!var_key)
+	{
+		result[(*j)++] = '$';
+		(*i)++;
+		return (1);
+	}
+	var_value = return_key_content(minishell, var_key);
+	if (var_value)
+	{
+		append_to_result(result, var_value, j);
+	}
+	*i += strlen(var_key) + 1;
+	free(var_key);
+	return (1);
+}
 
 char	*expand_token(t_mini *minishell, char *token)
 {
-	char	**temp;
-	char	*expanded;
-	char	*key_content;
-	char	*new_expanded;
+	char	*result;
 	int		i;
-	char	*dollar_pos;
+	int		j;
 
-	expanded = ft_strdup("");
-	if (!expanded)
-		return (NULL);
-	dollar_pos = ft_strchr(token, '$');
-	if (dollar_pos == NULL)
-		return (ft_strdup(token));
-	if (token != dollar_pos)
-	{
-		char *prefix = ft_substr(token, 0, dollar_pos - token);
-		new_expanded = ft_strjoin(expanded, prefix);
-		free(expanded);
-		expanded = new_expanded;
-		free(prefix);
-	}
-	temp = ft_split(dollar_pos, '$');
-	if (!temp)
-	{
-		free(expanded);
-		return (NULL);
-	}
+	result = malloc(sizeof(char) * calculate_size(minishell, token));
 	i = 0;
-	while (temp[i] != NULL)
+	j = 0;
+	if (!result)
+		return (NULL);
+	while (token[i])
 	{
-		key_content = test(minishell, temp[i]);
-		if (key_content != NULL)
-			new_expanded = ft_strjoin(expanded, key_content);
+		if (token[i] == '$')
+		{
+			if (is_valid(token[i + 1], 0))
+				expand_variable(minishell, result, &i, &j);
+			else
+			{
+				result[j++] = '$';
+				i++;
+			}
+		}
 		else
-			new_expanded = ft_strjoin(expanded, temp[i]);
-		free(expanded);
-		expanded = new_expanded;
-		if (key_content)
-			free(key_content);
-		i++;
+			result[j++] = token[i++];
 	}
-	free_matrix(temp);
-	return (expanded);
+	result[j] = '\0';
+	return (result);
 }
