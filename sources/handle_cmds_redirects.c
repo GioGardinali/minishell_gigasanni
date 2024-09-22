9112237f75ebd70c5a4b77512b446b1e6dea3e74
Giovanna Gardinali de Miranda <gigardin@student.42sp.org.br>
2024-09-22 17:46:07 -0300

    subindo ajustes de comandos, separando em novo arquivo handle

---
+++ sources/handle_cmds_redirects.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_cmds_redirects.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 17:42:06 by gigardin          #+#    #+#             */
+/*   Updated: 2024/09/22 17:43:11 by gigardin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void	add_redir(t_redir **redirs, int type, char *file)
+{
+	t_redir	*new_redir;
+	t_redir	*temp;
+
+	new_redir = malloc(sizeof(t_redir));
+	if (!new_redir)
+		return ;
+	new_redir->type = type;
+	new_redir->file = ft_strdup(file);
+	new_redir->next = NULL;
+	if (!*redirs)
+	{
+		*redirs = new_redir;
+		return ;
+	}
+	temp = *redirs;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_redir;
+}
+
+void	handle_redirection(t_token **token, t_cmd **cmd)
+{
+	int	type;
+
+	type = (*token)->type;
+	*token = (*token)->next;
+	if (*token != NULL)
+		add_redir(&(*cmd)->redirs, type, (*token)->str);
+	*token = (*token)->next;
+}
+
+void	handle_heredoc(t_token **token, unsigned int *count_cmd, t_cmd **cmd)
+{
+	if (!execute_heredoc((*token)->next->str, *count_cmd,
+			(*cmd)->heredocs, (*token)->prev == NULL))
+	{
+		ft_putendl_fd("Heredoc error", 2);
+		exit(EXIT_FAILURE);
+	}
+	*token = (*token)->next->next;
+}
--- sources/make_cmds.c
+++ sources/make_cmds.c
@@ -6,36 +6,11 @@
 /*   By: gigardin <gigardin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 16:39:02 by asanni            #+#    #+#             */
-/*   Updated: 2024/09/22 17:24:23 by gigardin         ###   ########.fr       */
+/*   Updated: 2024/09/22 17:43:27 by gigardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../include/minishell.h"
-
-void	add_redir(t_redir **redirs, int type, char *file);
-
-void	handle_redirection(t_token **token, t_cmd **cmd)
-{
-	int	type;
-
-	type = (*token)->type;
-	*token = (*token)->next;
-	if (*token != NULL)
-		add_redir(&(*cmd)->redirs, type, (*token)->str);
-	*token = (*token)->next;
-}
-
-void	handle_heredoc(t_token **token, unsigned int *count_cmd, t_cmd **cmd)
-{
-	if (!execute_heredoc((*token)->next->str, *count_cmd,
-			(*cmd)->heredocs, (*token)->prev == NULL))
-	{
-		ft_putendl_fd("Heredoc error", 2);
-		exit(EXIT_FAILURE);
-	}
-	*token = (*token)->next->next;
-}
 
 void	identify_type_cmd(t_token **token, t_cmd **cmd, char ***options,
 	unsigned int *count_cmd)
@@ -73,28 +48,6 @@ static char	**make_options(t_token **token, t_cmd **cmd,
 		identify_type_cmd(token, cmd, &options, count_cmd);
 	}
 	return (opt_bckp);
-}
-
-void	add_redir(t_redir **redirs, int type, char *file)
-{
-	t_redir	*new_redir;
-	t_redir	*temp;
-
-	new_redir = malloc(sizeof(t_redir));
-	if (!new_redir)
-		return ;
-	new_redir->type = type;
-	new_redir->file = ft_strdup(file);
-	new_redir->next = NULL;
-	if (!*redirs)
-	{
-		*redirs = new_redir;
-		return ;
-	}
-	temp = *redirs;
-	while (temp->next)
-		temp = temp->next;
-	temp->next = new_redir;
 }
 
 void	make_one_cmd(t_cmd **cmd, t_token **token, t_mini *minishell,
